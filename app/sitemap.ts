6ce89ea68f09ba81c80979d2b63aafddd51481f8
J. Vitor <34656666+metrix0@users.noreply.github.com>
2025-12-13 18:23:11 -0300

    Seo blog and optimization (#75)
    * fix lp video
    
    * seo blogs and optimization
    
    * ico fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,12 +2,12 @@ import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
-        { url: "https://imenuapp.com.br/", priority: 1 },
-        { url: "https://imenuapp.com.br/cardapio-digital", priority: 0.9 },
-        { url: "https://imenuapp.com.br/anota-ai", priority: 0.8 },
-        { url: "https://imenuapp.com.br/cardapio-digital-gratuito", priority: 0.8 },
-        { url: "https://imenuapp.com.br/saipos", priority: 0.7 },
-        { url: "https://imenuapp.com.br/goomer", priority: 0.7 },
-        { url: "https://imenuapp.com.br/gestor-de-pedidos", priority: 0.7 },
+        { url: "https://www.imenuapp.com.br/", priority: 1 },
+        { url: "https://www.imenuapp.com.br/cardapio-digital", priority: 0.9 },
+        { url: "https://www.imenuapp.com.br/anota-ai", priority: 0.8 },
+        { url: "https://www.imenuapp.com.br/cardapio-digital-gratuito", priority: 0.8 },
+        { url: "https://www.imenuapp.com.br/saipos", priority: 0.7 },
+        { url: "https://www.imenuapp.com.br/goomer", priority: 0.7 },
+        { url: "https://www.imenuapp.com.br/gestor-de-pedidos", priority: 0.7 },
     ];
 }
