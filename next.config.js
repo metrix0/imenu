797c3d6efb740cceb33592a214683137f232b6b4
João Vitor Redondano de Almeida <34656666+metrix0@users.noreply.github.com>
2026-04-03 11:14:13 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const { withSentryConfig } = require("@sentry/nextjs");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() = {
+  async redirects() {
     return [
       {
         source: '/saborosas-massas-947',
