63ba816dbfec742b652bf5b36233026f9f53f3c5
metrix0 <joaovralmeida@hotmail.com>
2026-08-03 17:56:25 -0300

    dev

--- app/(extra)/dev/layout.tsx
+++ app/(extra)/dev/layout.tsx
@@ -2,13 +2,13 @@
 import { ReactNode } from "react";
 
 export default function DevLayout({ children }: { children: ReactNode }) {
-    if (process.env.NODE_ENV !== "development") {
-        return (
-            <div style={{ padding: 50, textAlign: "center", color: "red" }}>
-                ❌ This page is for development only
-            </div>
-        );
-    }
+    // if (process.env.NODE_ENV !== "development") {
+    //     return (
+    //         <div style={{ padding: 50, textAlign: "center", color: "red" }}>
+    //             ❌ This page is for development only
+    //         </div>
+    //     );
+    // }
 
     return <>{children}</>;
 }
