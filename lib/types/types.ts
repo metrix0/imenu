0d3878fa1e9f4b481694839ed5f051ea830dda03
João Vitor Redondano de Almeida <34656666+metrix0@users.noreply.github.com>
2026-08-26 21:40:33 -0300

    support future scheduling and addon quantities

--- lib/types/types.ts
+++ lib/types/types.ts
@@ -31,6 +31,7 @@ export type Restaurant = {
     store_whatsapp?: string | null;
     allowed_payment_methods?: string[] | null;
     pickup_enabled?: boolean;
+    allow_future_order_scheduling?: boolean;
 };
 
 export type Menu = {
@@ -102,6 +103,7 @@ export type CartItem = {
         subitemId: string;
         subitemName: string;
         price_cents: number;
+        quantity?: number;
     }[];
     promotion?: Promotion;
 };
